9d0bba00aa93f708a5c07eca9010cf5827f8146b
Maikel <maikel_hennekes@hotmail.com>
2015-10-11 20:58:34 +0200

    PaymentFactoryTest assert toegevoegd die checkt of de createPayPalPayment en instantie van PayPalPayment retuneert!

--- Payment/src/test/java/nl/ead/webservice/business/entityServices/PaymentFactoryTest.java
+++ Payment/src/test/java/nl/ead/webservice/business/entityServices/PaymentFactoryTest.java
@@ -7,6 +7,7 @@ import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
 public class PaymentFactoryTest {
@@ -20,14 +21,11 @@ public class PaymentFactoryTest {
 
     @Test
     public void testPaymentFactory() throws Exception {
-//        assertEquals(PayPalPayment, paymentFactory.createPayPalPayment(1, 50, false, "paypalId"));
+        assertThat(paymentFactory.createPayPalPayment(1, "50", "USD", false, "paypalId"), instanceOf(PayPalPayment.class));
         assertEquals(1, paymentFactory.createPayPalPayment(1, "50", "USD", false, "paypalId").getUserId());
         assertEquals("50", paymentFactory.createPayPalPayment(1, "50", "USD", false, "paypalId").getAmount());
         assertEquals(false, paymentFactory.createPayPalPayment(1, "50", "USD", false, "paypalId").getPaymentConfirmed());
         assertEquals("paypalId", paymentFactory.createPayPalPayment(1, "50", "USD", false, "paypalId").getPaypalId());
         assertEquals("USD", paymentFactory.createPayPalPayment(1, "50", "USD", false, "paypalId").getCurrency());
-//      assertThat(paymentFactory.createPayPalPayment(1, 50, false, "paypalId"), isInstanceOf(PayPalPayment.class));
-
-        assertEquals(1, 1);
     };
 }
